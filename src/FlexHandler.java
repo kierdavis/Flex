79399c850fb943a472b6885df363a61be0d1b016
Kier Davis <kierdavis@gmail.con>
2013-06-12 15:51:06 +0100

    Fixed missing imports in FlexHandler

--- src/FlexHandler.java
+++ src/FlexHandler.java
@@ -1,5 +1,11 @@
 package com.kierdavis.flex;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
