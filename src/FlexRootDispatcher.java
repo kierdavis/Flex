470f9fc070d7bbdc0f3c9abbc0faa932c94243d9
Kier Davis <kierdavis@gmail.con>
2013-06-12 17:17:35 +0100

    Override dispatch in FlexRootDispatcher to handle the command name instead of popping arguments

--- src/FlexRootDispatcher.java
+++ src/FlexRootDispatcher.java
@@ -1,10 +1,29 @@
 package com.kierdavis.flex;
 
 public class FlexRootDispatcher extends FlexDispatcher {
+    @Override
     public void add(String[] path, FlexHandlingContext hctx) {
         super.add(path, hctx);
         
         String name = path[0];
         hctx.getPlugin().getServer().getPluginCommand(name).setExecutor(FlexCommandExecutor.getInstance());
+    }
+    
+    @Override
+    public boolean dispatch(FlexCommandContext ctx) {
+        String name = ctx.getCommand().getName().toLowerCase();
+        
+        if (children != null && children.containsKey(name)) {
+            FlexDispatcher child = children.get(name);
+            ctx.pushPathComponent(name);
+            return child.dispatch(ctx);
+        }
+        
+        //if (hctx != null) {
+        //    return hctx.invoke(ctx);
+        //}
+        
+        ctx.error("No such command '" + name + "'");
+        return false;
     }
 }
