ce2f67e123785676759940a6f9e94078eaca1da8
Kier Davis <kierdavis@gmail.con>
2013-06-13 16:52:34 +0100

    Document FlexHandler

--- src/FlexHandler.java
+++ src/FlexHandler.java
@@ -6,11 +6,36 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation used to mark methods on an object as being handlers for commands.
+ *
+ * Example:
+ * <pre>
+ * @FlexHandler("myplugin group add", permission = "myplugin.commands.add")
+ * public boolean handleAddGroup(FlexCommandContext ctx) {
+ *     // ...
+ * }
+ * </pre>
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface FlexHandler {
-    String value(); // Path
+    /**
+     * The command path (the top-level command followed by subcommand names), as
+     * a space-seperated string. It is named <code>value</code> to allow the
+     * element name to be omitted.
+     */
+    String value();
+    
+    /**
+     * If this is not the empty string, the command will fail if the sender does
+     * not have this permission.
+     */
     String permission() default "";
+    
+    /**
+     * If this is true, the command will fail if the sender is not a player.
+     */
     boolean playerOnly() default false;
 }
